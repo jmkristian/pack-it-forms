01d47039cec6abb6f0826854a2041d7fe630702f
John Kristian <jmkristian@gmail.com>
2019-09-23 14:28:48 -0700

    Add a simple form with its own installer.

--- resources/integration/scco/build.cmd
+++ resources/integration/scco/build.cmd
@@ -2,6 +2,21 @@
 
 @rmdir /Q /S built\addons
 @mkdir built\addons
+@copy "%~dp0add-on_example.nsi" built\addon.nsi
+@node bin/Outpost_Forms.js build %VersionMajor%.%VersionMinor% ^
+  add-on_example bin\Outpost_Forms.exe "Add-on Example"
+@if %errorlevel% neq 0 exit /b %errorlevel%
+@"C:\Program Files (x86)\NSIS\makensis.exe" ^
+  /DVersionMajor=%VersionMajor% ^
+  /DVersionMinor=%VersionMinor% ^
+  /Daddon_name=add-on_example ^
+  /DDisplayName="Add-on Example" ^
+  /DPROGRAM_PATH=bin\Outpost_Forms.exe ^
+  setup.nsi
+@if %errorlevel% neq 0 exit /b %errorlevel%
+
+@rmdir /Q /S built\addons
+@mkdir built\addons
 @copy "%~dp0Los_Altos.nsi" built\addon.nsi
 @node bin/Outpost_Forms.js build %VersionMajor%.%VersionMinor% ^
   Los_Altos bin\Outpost_Forms.exe "Outpost for LAARES"
